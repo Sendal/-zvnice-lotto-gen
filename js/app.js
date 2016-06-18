42bed397f21bb30c3fd80310f7dcda6cc608b8b0
Sendal <zvnice@nate.com>
2016-06-18 17:13:11 +0900

    버그 수정 - 중복된 번호 문제

--- js/app.js
+++ js/app.js
@@ -2,8 +2,21 @@ function generate() {
   var numbers = [];
 
   for(var i=0; i<6; i++) {
-    numbers.push(Math.floor(Math.random() * 45)+1);
+    var number = Math.floor(Math.random() * 45) +1;
+
+    if(contains(numbers, number)) {
+      i--;
+    } else {
+      numbers.push(number);
+    }
   }
 
   return numbers;
 }
+
+  function contains(numbers, number) {
+    for(var i = 0; i<numbers.length; i++) {
+      if(numbers[i] == number) return true;
+    }
+    return false;
+  }
